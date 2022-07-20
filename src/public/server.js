eeab130e526eaeaf517d69e8d3064a3f04a53a3d
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-20 17:10:52 +0530

    changes are done

--- src/public/server.js
+++ src/public/server.js
@@ -2,3 +2,14 @@ var PORT = process.env.PORT ||  8081;
 app.listen(PORT, "0.0.0.0", function() {
 // console.log("Listening on Port 3000");
 });
+
+
+
+// var distDir = __dirname + "/dist/";
+//  app.use(express.static(distDir));
+
+var distDir = __dirname + "./src/public/dist/sirius";
+ app.use(express.static(distDir));
+
+
+
