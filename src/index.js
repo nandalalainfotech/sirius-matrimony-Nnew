84d27fa78cd70e8c691ff0a2270c14799f5026a6
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-21 15:53:10 +0530

    changes are done

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 
 app.get('/', (req, res) => {
 
-    res.sendFile('index.html',{ root: path.resolve(path.dirname ('./src/public/dist/sirius'))});
+    res.sendFile('index.html', { root: path.resolve(path.dirname ('./src/public/dist/sirius'))});
 
 	// root: path.resolve(path.dirname
 });
